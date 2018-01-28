a6a76d9dc0d9bf1e7af078cdacef4568520213dc
y4izus <yaiza.garcia.mm@gmail.com>
2018-01-28 21:31:09 +0100

    Show the last image when the player get the logo

--- js/Game.js
+++ js/Game.js
@@ -6,10 +6,11 @@ function Game(canvas, ctx) {
   this.discoveredLevels = 0;
 
   this.player = new Player(canvas, ctx);
-  this.ball = new Ball(this.player.x, this.player.y, canvas, ctx);
-  this.bricks = this._createBricksArray();
+  this.showPlayer = true;
   this.movePlayer = false;
+  this.ball = new Ball(this.player.x, this.player.y, canvas, ctx);
   this.moveBall = false;
+  this.bricks = this._createBricksArray();
 
   this.showLogo = false;
   this.logo = {
@@ -21,6 +22,13 @@ function Game(canvas, ctx) {
   };
   this.logo.img.src =
     "https://avatars2.githubusercontent.com/u/2041155?s=200&v=4";
+
+    this.imgFinal = {
+      height: canvas.width * 0.35,
+      width: canvas.width * 0.35,
+      img: new Image()
+    }
+    this.imgFinal.img.src = "https://www.dropbox.com/s/es64182f9t25d2t/personwin.png?raw=1";
 }
 
 Game.prototype.start = function() {
@@ -35,13 +43,12 @@ Game.prototype.updateState = function() {
 
   if (this.moveBall) this.ball.move(this);
   this.ball.draw();
-  this.player.draw();
+  if(this.showPlayer) this.player.draw();
   this.bricks.forEach(e => e.draw());
   this._drawProgress();
 
   if (this.discoveredLevels === 3) {
     this._stop();
-    $("#ver-cv").removeAttr("disabled");
   }
   if (this.showLogo) {
     this.ctx.drawImage(
@@ -52,7 +59,20 @@ Game.prototype.updateState = function() {
       this.logo.height
     );
     this.logo.y += 7;
-    this.player.center();
+    if(this.showPlayer) this.player.center();
+  }
+
+  if (this.logo.y + this.logo.height > this.canvas.height - this.player.height) {
+    this.showPlayer = false;
+    this.showLogo = false;
+    this.ctx.drawImage(
+      this.imgFinal.img,
+      this.canvas.width - this.imgFinal.width,
+      this.canvas.height - this.imgFinal.height,
+      this.imgFinal.width,
+      this.imgFinal.height
+    );
+    $("#ver-cv").removeAttr("disabled");
   }
 };
 
